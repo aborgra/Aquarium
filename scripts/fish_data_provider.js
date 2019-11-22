62fe7610edde7be42e919d133b0ded8093ed2d09
aborgra <audrey.borgra@gmail.com>
2019-11-22 10:53:45 -0600

    saving before destruction

--- scripts/fish_data_provider.js
+++ scripts/fish_data_provider.js
@@ -4,41 +4,68 @@ const fishCollection = [
     Species: "Orange Clownfish",
     Location: "Caribbean",
     Length: 1,
-    Food: "Algae"
+    Food: ["Algae", "Crustaceans"],
+    image: "/images/bartpic.jpg"
   },
   {
     name: "Jerry",
     Species: "Yellow Fish",
     Location: "Pacific Ocean",
     Length: 2,
-    Food: "Boogers"
+    Food: "Boogers",
+    image: "/images/jerrypic.jpg"
   },
   {
     name: "Mike",
     Species: "Pink Fish",
     Location: "Arctic Ocean",
     Length: 3,
-    Food: "Ice"
+    Food: "Ice",
+    image: "/images/mikepic.jpg"
   },
   {
     name: "Noodle",
     Species: "Blue Fish",
     Location: "Dead Sea",
     Length: 4,
-    Food: "Salt"
+    Food: "Salt",
+    image: "/images/noodlepic.jpg"
   },
   {
     name: "Erroll",
     Species: "Rainbow Fish",
     Location: "Atlantic Ocean",
     Length: 5,
-    Food: "People"
+    Food: "People",
+    image: "/images/errollpic.jpg"
   },
   {
     name: "Sam",
     Species: "Beluga Fish",
     Location: "Caribbean",
     Length: 6,
-    Food: "Lobsters"
+    Food: "Lobsters",
+    image: "/images/sampic.jpg"
   },
 ]
+
+export const useFish = () => {
+  return fishCollection
+}
+
+
+
+// let addFish = (name, Species, Location, Length, Food) => {
+//   let newFish = {
+//     name: name,
+//     Species: Species,
+//     Location: Location,
+//     Length: Length,
+//     Food: Food
+//   }
+//   fishCollection.push(newFish);
+// }
+
+// addFish("Fluffy", "Brown Fish", "Deep", 8, "Water")
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,18 @@
 import initializeDetailButtonEvents from '/scripts/dialogs_main.js'
 import addMouseOverEventListeners from './highlightFishCards.js'
 import addTipsEventListener from './highlightTips.js'
+import { useFish } from './fish_data_provider.js'
 
 
+// newFunction()
 addTipsEventListener()
 addMouseOverEventListeners()
 initializeDetailButtonEvents()
+const allOfTheFish = useFish() //this var returns the value of the array bec the fx returns the value of the arr
+
+console.log("All of the fish:", allOfTheFish) //prints the array from fishCollection
+
+for (const fish of allOfTheFish) {
+  console.log("Individual fish object:", fish)
+}
+
